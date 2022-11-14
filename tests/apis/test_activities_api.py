5f5afd0becfe78d09da84d2af81f47ab6f7f0778
Ketai Qiu <ketai@usi-macbook-pro.home>
2022-11-14 09:58:00 +0100

    Adjust tests

--- tests/apis/test_activities_api.py
+++ tests/apis/test_activities_api.py
@@ -1,6 +1,8 @@
 from app.apis.activities_api import ActivitiesAPI
 from app.apis.identity_api_endpoint import IdentityAPIEndPoint
 from app.models.role import Role
+from app.models.activity.activity import Activity
+from app.utils.string import gen_random_description
 
 
 def test_get_activities():
@@ -21,8 +23,25 @@ def test_get_activities_by_query():
     assert res["totalCount"] >= 0
 
 
+def test_update_activity():
+    activities_api = ActivitiesAPI(None)
+    identity_api_endpoint = IdentityAPIEndPoint()
+    headers = identity_api_endpoint.get_headers(Role.ADMIN)
+    res = activities_api.get_activities_by_query(
+        headers,
+        {
+            "requireTotalCount": True,
+            "filter": f'["code","=","AAS246"]',
+        },
+    )
+    activity_to_update = Activity(res["data"][0])
+    activity_to_update.description = gen_random_description()
+    activities_api.update_activity(headers, activity_to_update)
+
+
 def main():
-    test_get_activities_by_query()
+    # test_get_activities_by_query()
+    test_update_activity()
 
 
 if __name__ == "__main__":
