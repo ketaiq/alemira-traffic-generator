9cb6faa7555d24d890af16c4fd7d7264a84975cf
Ketai Qiu <ketai.qiu@vwic233.home>
2023-02-09 04:11:44 +0100

    Update action

--- tests/test_locustfile.py
+++ tests/test_locustfile.py
@@ -1,10 +1,4 @@
 import pandas as pd
-import app.locusttasks.instructor_tasks as instructor_tasks
-import app.locusttasks.student_tasks as student_tasks
-from app.locusttasks.stages import Stage
-import app.locusttasks.weights as weights
-import inspect
-from app.locusttasks.stages import Stage
 
 
 def test_StagesShape_init():
@@ -28,25 +22,8 @@ def test_StagesShape_init():
     }
 
 
-def test_generate_tasks(stage: str) -> list:
-    tasks = []
-    for name, func in inspect.getmembers(instructor_tasks, inspect.isfunction):
-        tasks += (
-            [func]
-            * weights.INSTRUCTOR_TASK_WEIGHTS[stage][name]
-            * weights.INSTRUCTOR_WEIGHTS[stage]
-        )
-    for name, func in inspect.getmembers(student_tasks, inspect.isfunction):
-        tasks += (
-            [func]
-            * weights.STUDENT_TASK_WEIGHTS[stage][name]
-            * weights.STUDENT_WEIGHTS[stage]
-        )
-
-
 def main():
-    # test_StagesShape_init()
-    test_generate_tasks(Stage.FIRST.value)
+    test_StagesShape_init()
 
 
 if __name__ == "__main__":
